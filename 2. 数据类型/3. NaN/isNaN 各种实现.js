bdcfe5d722e6753ab9270c8b6deb48c2743533c0
zhangzhengyang <1074385735@qq.com>
2022-07-11 12:12:20 +0800

    NaN

--- 2. 数据类型/3. NaN/isNaN 各种实现.js
+++ 2. 数据类型/3. NaN/isNaN 各种实现.js
@@ -1,3 +1,4 @@
+// 判断NaN的各种实现
 function isNaNVal(val) {
     return Object.is(val, NaN);
 }
@@ -12,7 +13,23 @@ function isNaNVal(val) {
 
 // 综合垫片
 if (!("isNaN" in Number)) {
-    Number.isNaN = function(val) {
+    Number.isNaN = function (val) {
         return typeof val === 'number' && isNaN(val)
     }
 }
+
+/*
+* typeof 是数字
+* 我不能等于我自己
+* 不能被删除
+* */
+
+// isNaN:检查toNumber返回值，如果是NAN返回true
+
+var arr = [NaN];
+console.log(arr.indexOf(NaN))   // -1
+console.log(arr.includes(NaN))  // true
+
+// includes:调用内部的Number::sameValueZero
+// indexOf:调用内部的Number::equal
+
