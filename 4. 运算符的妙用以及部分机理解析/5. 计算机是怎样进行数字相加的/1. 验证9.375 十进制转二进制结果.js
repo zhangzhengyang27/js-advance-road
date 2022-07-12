748a1aac8f0a3bab1720e179a245a2586184e6ed
zhangzhengyang <1074385735@qq.com>
2022-07-12 09:58:40 +0800

    0.1+0.2的问题

--- 4. 运算符的妙用以及部分机理解析/5. 计算机是怎样进行数字相加的/1. 验证9.375 十进制转二进制结果.js
+++ 4. 运算符的妙用以及部分机理解析/5. 计算机是怎样进行数字相加的/1. 验证9.375 十进制转二进制结果.js
@@ -1,4 +1,21 @@
-console.log((9.375).toString(2))
-console.log(Number.prototype.toString.call(9.375, 2))
-console.log(Number.prototype.toString.call(Number(9.375), 2))
+/*
+十进制和二进制是如何相互转换的
+浮点数是以什么格式存储的
+计算机是怎么进行加法的
+* */
+
+
+// 十进制和二进制是如何相互转换的
+//  十进制--->二进制
+//  整数:除2取余，逆序排列
+//  小数:乘2取整，顺序排列
+
+//  二进制--->十进制
+// 小数点前：从右到左用二进制的每个数乘以2的相应次方递增  0 1 2 3
+// 小数点后：从左到右用二进制的每个数乘以2的相应负次方递增 -1 -2 -3
+
+
+console.log((9.375).toString(2))  // 1001.011
+console.log(Number.prototype.toString.call(9.375, 2))  // 1001.011
+console.log(Number.prototype.toString.call(Number(9.375), 2))  // 1001.011
 
--- 4. 运算符的妙用以及部分机理解析/5. 计算机是怎样进行数字相加的/2. 二进制转10.js
+++ 4. 运算符的妙用以及部分机理解析/5. 计算机是怎样进行数字相加的/2. 二进制转10.js
@@ -1,2 +1,4 @@
-
 console.log((0.30000000000000004).toString(2))
+
+// 不能这样写
+console.log((1001.011).toString(10))
